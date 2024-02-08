f3c1684ea9f75193148a95a8a47d6a4d3e79ee1c
SZucchini <suzuki.tomohiro@g.sp.m.is.nagoya-u.ac.jp>
2024-02-08 17:25:51 +0900

    pose estimation

---
+++ backend/pose/run.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from mmdet.apis import init_detector
+from mmpose.apis import init_model
+from mmpose.utils import adapt_mmdet_pipeline
+
+from .calib.calibration import calibrate
+from .inference import inferencer
+from .MotionAGFormer.model import MotionAGFormer
+from .vis.calibpose import vis_calib_res
+
+
+def load_models(device):
+    det_config = './mmpose/mmdet_cfg/rtmdet_m_640-8xb32_coco-person.py'
+    det_ckpt = 'https://download.openmmlab.com/mmpose/v1/projects/rtmpose/rtmdet_m_8xb32-100e_coco-obj365-person-235e8209.pth'
+    detector = init_detector(det_config, det_ckpt, device=device)
+    detector.cfg = adapt_mmdet_pipeline(detector.cfg)
+
+    pose_config = './mmpose/mmpose_cfg/td-hm_ViTPose-huge_8xb64-210e_coco-256x192.py'
+    pose_ckpt = 'https://download.openmmlab.com/mmpose/v1/body_2d_keypoint/topdown_heatmap/coco/td-hm_ViTPose-huge_8xb64-210e_coco-256x192-e32adcd4_20230314.pth'
+    pose_estimator = init_model(pose_config, pose_ckpt, device=device)
+
+    model_path = "./MotionAGFormer/checkpoint/motionagformer-xs-h36m.pth.tr"
+    if model_path.split('/')[-1].split('-')[1] == 'l':
+        pose_lifter = MotionAGFormer(n_layers=26, dim_in=3, dim_feat=128,
+                                     num_heads=8, neighbour_num=2)
+    elif model_path.split('/')[-1].split('-')[1] == 'b':
+        pose_lifter = MotionAGFormer(n_layers=16, dim_in=3, dim_feat=128,
+                                     num_heads=8, neighbour_num=2)
+    elif model_path.split('/')[-1].split('-')[1] == 'xs':
+        pose_lifter = MotionAGFormer(n_layers=12, dim_in=3, dim_feat=64,
+                                     num_heads=8, neighbour_num=2, n_frames=27)
+    pose_lifter = nn.DataParallel(pose_lifter)
+    pre_dict = torch.load(model_path)
+    pose_lifter.load_state_dict(pre_dict['model'], strict=True)
+    pose_lifter.eval()
+    pose_lifter.to(device)
+
+    return detector, pose_estimator, pose_lifter
+
+
+def main():
+    # 3視点からの動画含まれるフォルダ
+    video_folder = "./sample_video"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    detector, pose_estimator, pose_lifter = load_models(device)
+    kpts2d, scores2d, kpts3d, scores3d = inferencer(video_folder,
+                                                    detector, pose_estimator, pose_lifter, device)
+
+    # カメラによって異なるので今後変更が必要
+    param_c13 = np.load("./intrinsic/iphone13.npz")
+    param_c2 = np.load("./intrinsic/iphone11pro.npz")
+    K = np.array([param_c13["mtx"], param_c2["mtx"], param_c13["mtx"]])
+
+    R_est, t_est, kpts3d_est, kpts3d_tri = calibrate(kpts2d, scores2d, kpts3d, scores3d, K)
+    ani = vis_calib_res(kpts3d_tri, kpts3d)
+
+    save_path = "./sample_output/result.gif"
+    ani.save(save_path, writer="pillow")
+
+
+if __name__ == "__main__":
+    main()
---
+++ backend/pose/vis/bone.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+H36M_KEY = {
+    "MidHip": 0,
+    "RHip": 1,
+    "RKnee": 2,
+    "RAnkle": 3,
+    "LHip": 4,
+    "LKnee": 5,
+    "LAnkle": 6,
+    "Spine": 7,
+    "Thorax": 8,
+    "Nose": 9,
+    "Head": 10,
+    "LShoulder": 11,
+    "LElbow": 12,
+    "LWrist": 13,
+    "RShoulder": 14,
+    "RElbow": 15,
+    "RWrist": 16,
+}
+
+H36M_BONE = np.array(
+    [
+        [H36M_KEY["Head"], H36M_KEY["Nose"]],
+        [H36M_KEY["Nose"], H36M_KEY["Thorax"]],
+        [H36M_KEY["Thorax"], H36M_KEY["Spine"]],
+        [H36M_KEY["Thorax"], H36M_KEY["RShoulder"]],
+        [H36M_KEY["Thorax"], H36M_KEY["LShoulder"]],
+        [H36M_KEY["RShoulder"], H36M_KEY["RElbow"]],
+        [H36M_KEY["LShoulder"], H36M_KEY["LElbow"]],
+        [H36M_KEY["RWrist"], H36M_KEY["RElbow"]],
+        [H36M_KEY["LWrist"], H36M_KEY["LElbow"]],
+        [H36M_KEY["Spine"], H36M_KEY["MidHip"]],
+        [H36M_KEY["RHip"], H36M_KEY["MidHip"]],
+        [H36M_KEY["LHip"], H36M_KEY["MidHip"]],
+        [H36M_KEY["RHip"], H36M_KEY["RKnee"]],
+        [H36M_KEY["RKnee"], H36M_KEY["RAnkle"]],
+        [H36M_KEY["LHip"], H36M_KEY["LKnee"]],
+        [H36M_KEY["LKnee"], H36M_KEY["LAnkle"]],
+    ],
+    dtype=np.int64,
+)
+
+H36M_ALIGNED_KEY = {
+    "MidHip": 0,
+    "RHip": 1,
+    "RKnee": 2,
+    "RAnkle": 3,
+    "LHip": 4,
+    "LKnee": 5,
+    "LAnkle": 6,
+    "Thorax": 7,
+    "Nose": 8,
+    "LShoulder": 9,
+    "LElbow": 10,
+    "LWrist": 11,
+    "RShoulder": 12,
+    "RElbow": 13,
+    "RWrist": 14,
+}
+
+H36M_ALIGNED_BONE = np.array(
+    [
+        [H36M_ALIGNED_KEY["Nose"], H36M_ALIGNED_KEY["Thorax"]],
+        [H36M_ALIGNED_KEY["Thorax"], H36M_ALIGNED_KEY["RShoulder"]],
+        [H36M_ALIGNED_KEY["Thorax"], H36M_ALIGNED_KEY["LShoulder"]],
+        [H36M_ALIGNED_KEY["RShoulder"], H36M_ALIGNED_KEY["RElbow"]],
+        [H36M_ALIGNED_KEY["LShoulder"], H36M_ALIGNED_KEY["LElbow"]],
+        [H36M_ALIGNED_KEY["RWrist"], H36M_ALIGNED_KEY["RElbow"]],
+        [H36M_ALIGNED_KEY["LWrist"], H36M_ALIGNED_KEY["LElbow"]],
+        [H36M_ALIGNED_KEY["Thorax"], H36M_ALIGNED_KEY["MidHip"]],
+        [H36M_ALIGNED_KEY["RHip"], H36M_ALIGNED_KEY["MidHip"]],
+        [H36M_ALIGNED_KEY["LHip"], H36M_ALIGNED_KEY["MidHip"]],
+        [H36M_ALIGNED_KEY["RHip"], H36M_ALIGNED_KEY["RKnee"]],
+        [H36M_ALIGNED_KEY["RKnee"], H36M_ALIGNED_KEY["RAnkle"]],
+        [H36M_ALIGNED_KEY["LHip"], H36M_ALIGNED_KEY["LKnee"]],
+        [H36M_ALIGNED_KEY["LKnee"], H36M_ALIGNED_KEY["LAnkle"]],
+    ],
+    dtype=np.int64,
+)
