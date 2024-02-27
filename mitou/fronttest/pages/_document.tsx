e007d385692536a308c8356f27247c8e25fc0af5
konayuki1069 <konaka.yuki.i0@s.mail.nagoya-u.ac.jp>
2024-02-27 23:06:40 +0900

    wsl

---
+++ mitou/fronttest/pages/_document.tsx
@@ -0,0 +1,21 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Passion Oneフォントを読み込む */}
+          <link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
